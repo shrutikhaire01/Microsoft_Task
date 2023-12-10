84f4b699cf23d3c8f9ff0ec7e2da7babc41312a8
Shruti Khaire <111962097+shrutikhaire01@users.noreply.github.com>
2023-12-10 14:08:32 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Shruti Kishor Khaire",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "SHRUTI.KishorKhaire@studentambassadors.com",
+  gitHub: "shrutikhaire01",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/leaves-8413064_1280.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "leaves abstract background";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Blog Website🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Visit My Blog Website, I write about tech and my experience.",
+    url: "https://shrutikhaire01.github.io/Tribute-Page/",
   },
   {
     title: "Web Development for Beginners",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "You can find Easy Web Development Projects for Beginners.",
+    url: "https://shrutikhaire01.github.io/TemperatureConvertor/",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created my Resume Site which includes my experience and Academics.",
+    url: "https://shrutikhaire01.github.io/Portfolio_Basic/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Basic of HTML",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Video Getting Started With HTML.",
+    url: "https://youtu.be/3VpZFFn0wiA?si=rv47y-41Bq9o51dk",
   },
 ];
 
